f7a02ebb847dee9027f69f573c392a5111812ecd
FdelMazo <FdelMazo@fi.uba.ar>
2022-09-11 21:22:25 -0300

    Initial Commit -> Chakra UI + React Leaflet

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
 import React from 'react';
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
+import 'leaflet-defaulticon-compatibility';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <MapContainer
+        center={{ lat: -34.6037, lng: -58.3816 }} // Obelisco
+        zoom={15}
+        style={{ height: "100vh", width: "100vw" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[-34.6037, -58.3816]}>
+          <Popup>
+            Obelisco!
+          </Popup>
+        </Marker>
+      </MapContainer>
     </ChakraProvider>
   );
 }
