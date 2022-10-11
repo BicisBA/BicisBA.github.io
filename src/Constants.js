0dae3bac092d094b1db81f18456b3fdcb0ea67e8
FdelMazo <FdelMazo@fi.uba.ar>
2022-10-11 09:15:17 -0300

    Use new heroku backend

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,3 @@
 export const CABA_BOUNDS = [[-34.524197, -58.552391], [-34.715765, -58.265415]] // A manopla
 export const OBELISCOU = { lat: -34.6037, lng: -58.3816 }
-export const BACKEND = 'http://192.168.1.222:10101'
+export const BACKEND = 'https://bicisba-frame.herokuapp.com'
