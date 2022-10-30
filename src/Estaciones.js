4557c861e035356efad03aca648d5713ff8762cb
FdelMazo <FdelMazo@fi.uba.ar>
2022-10-30 16:38:33 -0300

    Dont hide popover w/parent padding

--- src/Estaciones.js
+++ src/Estaciones.js
@@ -52,7 +52,7 @@ function Estacion({ station_id }) {
 
   return (
     <>
-      <Flex direction={'row'} justifyContent="space-between" my={1}>
+      <Flex px={4} direction={'row'} justifyContent="space-between" my={1}>
         <Stat size="sm" textAlign="left">
           <StatNumber>{estacion.name.split('-')[1]}</StatNumber>
           <StatHelpText m={0}>Estación a {Math.round(nearestEstaciones[station_id].distance, 2)} metros</StatHelpText>
@@ -90,15 +90,15 @@ function Estaciones() {
   }, [spaceBar]);
 
   return (
-    <Box position="absolute" color="black" bottom="0" zIndex={1000} w="100%" bg="brown.50" pt={4} px={4} rounded="lg" borderTop="2px solid" borderColor="brown.400">
+    <Box position="absolute" color="black" bottom="0" zIndex={1000} w="100%" bg="brown.50" pt={4} rounded="lg" borderTop="2px solid" borderColor="brown.400">
       <Flex direction={'row'} w="100%" justifyContent="space-between" onClick={() => setExpand(!expand)} cursor="pointer" my={2}>
-        <Heading display="flex">
+        <Heading ml={4} display="flex">
           Estaciones
           <Icon viewBox="0 0 640 512" ml={2}>
             <path fill="currentColor" d="M312 32c-13.3 0-24 10.7-24 24s10.7 24 24 24h25.7l34.6 64H222.9l-27.4-38C191 99.7 183.7 96 176 96H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h43.7l22.1 30.7-26.6 53.1c-10-2.5-20.5-3.8-31.2-3.8C57.3 224 0 281.3 0 352s57.3 128 128 128c65.3 0 119.1-48.9 127-112h49c8.5 0 16.3-4.5 20.7-11.8l84.8-143.5 21.7 40.1C402.4 276.3 384 312 384 352c0 70.7 57.3 128 128 128s128-57.3 128-128s-57.3-128-128-128c-13.5 0-26.5 2.1-38.7 6L375.4 48.8C369.8 38.4 359 32 347.2 32H312zM458.6 303.7l32.3 59.7c6.3 11.7 20.9 16 32.5 9.7s16-20.9 9.7-32.5l-32.3-59.7c3.6-.6 7.4-.9 11.2-.9c39.8 0 72 32.2 72 72s-32.2 72-72 72s-72-32.2-72-72c0-18.6 7-35.5 18.6-48.3zM133.2 368h65c-7.3 32.1-36 56-70.2 56c-39.8 0-72-32.2-72-72s32.2-72 72-72c1.7 0 3.4 .1 5.1 .2l-24.2 48.5c-9 18.1 4.1 39.4 24.3 39.4zm33.7-48l50.7-101.3 72.9 101.2-.1 .1H166.8zm90.6-128H365.9L317 274.8 257.4 192z" />
           </Icon>
         </Heading>
-        {expand ? <ArrowRightIcon transform="rotate(90deg)" boxSize={8} /> : <ArrowLeftIcon transform="rotate(90deg)" boxSize={8} />}
+        {expand ? <ArrowRightIcon mr={4} transform="rotate(90deg)" boxSize={8} /> : <ArrowLeftIcon mr={4} transform="rotate(90deg)" boxSize={8} />}
       </Flex>
 
       <Collapse startingHeight={62} in={expand}>
@@ -111,7 +111,7 @@ function Estaciones() {
         }).map(({ station_id }) => (
           <Estacion station_id={station_id} key={station_id} />
         ))}
-        <Flex justifyContent="flex-end">
+        <Flex mr={4} justifyContent="flex-end">
           <Link isExternal={true} href="https://github.com/BicisBA/BicisBA.github.io">
             <Icon my={1} color="brown.600" boxSize={7} viewBox="0 0 496 512">
               <path
