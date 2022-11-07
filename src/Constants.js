4bd802c7463507f731c2eb94ab170993f47ea81b
CrossNox <ijmermet@gmail.com>
2022-11-07 06:13:57 -0300

    use dokku deploy

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,6 @@
 export const CABA_BOUNDS = [[-34.524197, -58.552391], [-34.715765, -58.265415]] // A manopla
 export const OBELISCOU = { lat: -34.6037, lng: -58.3816 }
-export const BACKEND = 'https://bicisba-frame.herokuapp.com'
+export const BACKEND = 'http://frame.dokku.crossnox.dev'
 
 export const RESULTS = {
     green: {
